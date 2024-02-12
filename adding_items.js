7e8a680658102afad314e5f6879205b6a98a1fd4
Vignesh1hp <vigneshkumarb.sc2004@gmail.com>
2024-02-12 23:24:45 +0530

    feat: added files and its unfinished

---
+++ adding_items.js
@@ -0,0 +1,18 @@
+const user_input = document.getElementById('userInput');
+
+const todoList = document.getElementById('todoList');
+
+user_input.addEventListener('keypress', (e) => {
+  if (e.code === "Enter") {
+    console.log('hello');
+    let todo = e.target.value;
+    let list_item = document.createElement('li');
+    let todoId = new Date().getTime();
+    list_item.innerHTML = `
+      <input type="checkbox" id="${todoId}"/>
+      <label for="${todoId}">${todo}</label>
+    `;
+    todoList.appendChild(list_item);
+    user_input.value = '';
+  }
+})
