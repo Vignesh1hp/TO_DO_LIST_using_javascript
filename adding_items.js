2fc214f751a0dea2075bd7df7db34a684cb8bc21
Vignesh1hp <vigneshkumarb.sc2004@gmail.com>
2024-02-14 16:36:21 +0530

    feat:added js code

--- adding_items.js
+++ adding_items.js
@@ -3,7 +3,7 @@ const user_input = document.getElementById('userInput');
 const todoList = document.getElementById('todoList');
 
 user_input.addEventListener('keypress', (e) => {
-  if (e.code === "Enter" && user_input !== '') {
+  if (e.code === "Enter" && e.target.value !== '') {
     console.log('hello');
     let todo = e.target.value;
     let list_item = document.createElement('li');
@@ -15,4 +15,6 @@ user_input.addEventListener('keypress', (e) => {
     todoList.appendChild(list_item);
     user_input.value = '';
   }
+
+
 })
